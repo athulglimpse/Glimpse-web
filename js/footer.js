c62785f20d7aa322f39feb62b452ab93f830fe43
Marwan Bassam <marwanbassam13@gmail.com>
2023-05-10 12:48:47 +0400

    Added 2 new universal contact btns

--- js/footer.js
+++ js/footer.js
@@ -171,16 +171,51 @@ footer.innerHTML = `
 </footer>
     `;
 
-let count = 0;
-document.getElementsByClassName("footer-logo")[0].addEventListener("click", function (e) {
-  e.preventDefault();
-  count++;
-
-  if (count == 6) {
-    window.open("https://youtu.be/xvFZjo5PgG0");
-    count = 0;
-  }
-}, false);
+// Adding extra floating buttons
+window.onload = () => {
+  var extraButtons = document.createElement("div");
+  extraButtons.style = "position: fixed; right: 20px; bottom: 90px;"
+  extraButtons.innerHTML = `
+  <div id="wtsp-btn">
+    <a target="_blank" href="https://wa.me/971524553543?text=Hey!%20I%20would%20like%20to%20enquire%20about%20ma%20ass%20%3A)" style="position: relative; border-radius: 100px; height: 50px; width: 50px; display: block; right: -200px;">
+    <svg width="50" height="50" viewBox="-400 -425 800 800" xml:space="preserve">
+    <g>
+      <path fill="#0D9F16" d="M398.75-24.55C398.75,194.91,220.8,373,1.33,373c-219.52,0-397.42-178.09-397.42-397.55
+        S-218.19-422,1.33-422C220.8-422,398.75-244.01,398.75-24.55z"/>
+        <g>
+          <path fill-rule="evenodd" clip-rule="evenodd" fill="#FCFCFC" d="M231.32-79.93C205.41-187.42,112.93-260.75,1.15-262.01
+            c-14.72,0.24-30.13,1.45-45.37,4.65C-200.16-225.79-280.96-52.9-204.95,86.69c1.96,3.74,2.11,6.76,1.03,10.62
+            c-10.11,36.8-20.12,73.6-30.19,110.27c-1.63,6.04-1.63,6.04,4.4,4.47c38.16-10.01,76.25-19.91,114.38-30.04
+            c3.16-0.85,5.64-0.49,8.53,0.96c49.11,25.09,100.84,32.46,155.01,21.96C177.63,179.84,262.21,48.2,231.32-79.93z M34.15,167.17
+            c-10.65,1.81-21.35,2.77-26.99,2.66c-41.47-0.12-75.29-9.41-106.39-28.23c-3.98-2.42-7.45-3.02-12.1-1.81
+            c-20.9,5.79-41.96,10.98-62.89,16.52c-4.4,1.33-4.82,0.36-3.71-3.62c5.55-19.9,10.77-39.69,16.5-59.36
+            c1.59-5.55,1.03-9.77-2.14-14.72c-43.01-69.13-41.2-155.27,6.94-219.22c50.01-66.6,118-93.86,199.35-76.49
+            c80.17,16.89,131.27,68.65,150.06,148.4C218.44,40.36,144.42,148.83,34.15,167.17z"/>
+          <path fill-rule="evenodd" clip-rule="evenodd" fill="#FDFDFD" d="M62.86,86.94c-8.41,0.36-16.44-1.57-24.49-3.74
+            C-15.77,67.63-56.4,33.73-89.04-10.91c-12.34-16.77-23.76-34.14-27.56-55.02c-4.56-25.1,2.65-46.33,21.32-63.71
+            c5.94-5.43,24.04-7.84,31.28-4.34c2.69,1.33,4.4,3.74,5.52,6.28c6.84,16.29,13.6,32.33,20.3,48.62c1.27,3.14,0.72,6.15-0.69,9.29
+            c-3.44,7.48-8.96,13.51-14.57,19.42c-4.25,4.35-4.44,8.33-1.3,13.52C-35.62-5-9.17,18.16,25.43,32.04
+            c5.16,2.05,9.05,1.33,12.48-3.01c5.76-7.12,11.85-14.24,17.31-21.6c3.74-5.19,8.02-5.91,13.3-3.5
+            c11.43,5.19,22.86,10.62,34.3,16.05c2.56,1.09,5.1,2.42,7.63,3.62c11.58,5.79,11.47,5.91,10.47,18.7
+            c-1.93,24.97-20.66,37.29-42.86,43.44C73.18,87.05,67.99,87.18,62.86,86.94z"/>
+        </g>
+      </g>
+    </svg>
+    </a>
+  </div>
+  <div id="call-btn" style="margin-top: 20px;">
+    <a href="tel:+97145655290" style="border-radius: 100px; height: 50px; width: 50px; display: block; position: relative; right: -200px;">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="50" height="50" viewBox="0 0 256 256" xml:space="preserve">
+      <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+        <circle cx="45" cy="45" r="45" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(33,150,243); fill-rule: nonzero; opacity: 1;" transform="  matrix(1 0 0 1 0 0) "/>
+        <path d="M 57.332 70.088 c -1.955 0.001 -4.074 -0.313 -6.32 -0.946 c -6.303 -1.777 -12.904 -5.885 -18.586 -11.568 c -5.682 -5.682 -9.79 -12.283 -11.568 -18.586 c -1.84 -6.528 -0.978 -11.999 2.429 -15.406 c 0.124 -0.124 0.25 -0.244 0.38 -0.36 c 0.033 -0.046 0.07 -0.09 0.112 -0.131 l 1.819 -1.819 c 0.877 -0.877 2.042 -1.36 3.282 -1.36 c 1.24 0 2.405 0.483 3.282 1.36 l 6.976 6.977 c 1.809 1.809 1.809 4.754 0 6.563 L 36.149 37.8 c -0.753 0.753 -0.753 1.979 0 2.732 L 49.468 53.85 c 0.729 0.729 2.002 0.729 2.732 0 l 2.988 -2.988 c 1.809 -1.808 4.755 -1.81 6.564 -0.001 l 6.977 6.977 c 0.876 0.876 1.359 2.042 1.359 3.281 s -0.483 2.405 -1.359 3.281 l -1.819 1.819 c -0.042 0.042 -0.087 0.08 -0.133 0.114 c -0.117 0.13 -0.236 0.256 -0.36 0.379 C 64.182 68.948 61.058 70.088 57.332 70.088 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+      </g>
+    </svg>
+    </a>
+  </div>
+  `;
+  document.getElementsByTagName("body")[0].appendChild(extraButtons);
+}
 
 // CHATWOOT
 (function (d, t) {
@@ -197,3 +232,4 @@ document.getElementsByClassName("footer-logo")[0].addEventListener("click", func
     })
   }
 })(document, "script");
+
