3b7107123161ddf9e1d0bdb82c26fff3480bc368
Marwan Bassam <marwanbassam13@gmail.com>
2023-06-19 13:40:51 +0400

    SEO fixes

--- js/parallax-slider.js
+++ js/parallax-slider.js
@@ -110,30 +110,6 @@ function animateImage() {
 }
 animate();
 
-// CLICK EVENT
-function slideClick(id) {
-  let overlay_bg = overlay.querySelector(".fullview__portfolio");
-  let title = overlay.querySelector(".fullview__portfolio-title");
-  title.innerHTML = titles[id - 1];
-  //   overlay.style.display = "block";
-  gsap.to(overlay, {
-    display: "block",
-    opacity: 1,
-    transform: "scale(1)",
-    ease: "power2.out",
-    duration: 0.6,
-  });
-  gsap.from(".fullview__portfolio-title", {
-    duration: 1.5,
-    yPercent: 200,
-    opacity: 0,
-    ease: "power4",
-    stagger: 0.1,
-    delay: 0.2,
-  });
-  overlay_bg.style.backgroundImage = `url(https://d3i7ke5fj3tp7c.cloudfront.net/images/portfolio/${id}.webp)`;
-}
-
 window.addEventListener("scroll", () => {
   if (overlay.style.display === "block") {
     // gsap
@@ -155,25 +131,3 @@ window.addEventListener("scroll", () => {
     });
   }
 });
-
-function slideClose() {
-  if (overlay.style.display === "block") {
-    // gsap
-    //   .from(".fullview__portfolio-title", {
-    //     duration: 1.5,
-    //     yPercent: -200,
-    //     ease: "power4",
-    //     stagger: 0.1,
-    //   })
-    //   .revert();
-
-    gsap.to(overlay, {
-      transform: "scale(0)",
-      display: "none",
-      opacity: 0,
-      ease: "power4",
-      duration: 0.6,
-      delay: 0,
-    });
-  }
-};
